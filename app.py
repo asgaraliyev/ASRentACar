1b3490ebde69653f57e5397271aecebe0fb0bf44
asgaraliyev <asgaraliyev2001@gmail.com>
2020-06-07 17:42:24 +0400

    bir var yox sey oldu nem cars.json oldu car a girib baxa bilirsen bir bir nem soram cars.html de sekillere baxa bilirsen json faylinan uhuuu

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask,render_template,url_for
+import json
 app = Flask(__name__)
 @app.route("/cars")
 def cars():
-    return render_template("cars.html")
+    with open("cars.json","r") as file:
+        data=file.read()
+    obyekt=json.loads(data)
+    
+    return render_template("cars.html",cars=obyekt)
 @app.route("/about")
 def about():
     return render_template("about.html")    
+@app.route("/car/<int:id>")
+def car(id):
+    with open("cars.json","r") as file:
+        data=file.read()
+    obyekt=json.loads(data)
+    return render_template("car.html",id=id,cars=obyekt)        
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
