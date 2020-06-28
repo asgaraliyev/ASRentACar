5b1608dbf41cdb7ed138920812267fa05af18037
asgaraliyev <asgaraliyev@gmail.com>
2020-06-28 19:06:41 +0400

    track modifications yaddan cxmiwdi

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ app = Flask(__name__)
 app.config['BABEL_DEFAULT_LOCALE']='az'
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
